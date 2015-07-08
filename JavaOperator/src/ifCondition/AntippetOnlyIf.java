b19f1965ed121416974307a5276bdc5e6c2400a5
자바오후 <@itbank_12-PC>
2015-07-08 15:19:36 +0900

    2015.07.08.자바학습3일차(연산자)

---
+++ JavaOperator/src/ifCondition/AntippetOnlyIf.java
@@ -0,0 +1,33 @@
+package ifCondition;
+
+/*
+ * @Date : 2015.07.08
+ * @ Author : me
+ * @Stroy : If�� �ִ� ����
+ */
+public class AntippetOnlyIf {
+	public static void main(String[] args) {
+		/*
+		 * ���� (statement
+		 * �ڹ� ����Ŀ��� 3���� ������ �������� ������.
+		 * ���ǹ�
+		 * if ,if-else ,switch
+		 * 
+		 * �ݺ���
+		 * while ,for ,do-while
+		 * 
+		 * ��Ÿ��
+		 * break, continue, return
+		 */
+
+		int x =1, y=2;
+		if (x==y) { // ���� �� ������ ==0
+			System.out.println("���� x �� y�� ���� ����");
+		}
+		if(x!=y){
+			System.out.println("���� x �� y�� ���� ���� �ʴ�");
+		}
+	
+	}
+
+}
--- JavaOperator/src/ifCondition/IfElse.java
+++ JavaOperator/src/ifCondition/IfElse.java
@@ -1,14 +1,17 @@
 package ifCondition;
 
-public class OnlyIf {
+public class IfElse {
 
 	public static void main(String[] args) {
 		int x = 1, y =2; // 지변 초기화
 		if ((x==1) && (y==2)) { // && 엔퍼센트라고 읽고 and연산자이다.
 			System.out.println("x는 1이면서 y도 2이다");
 		}
-		if ((x==1) || (y==2) ) { // || 이거는 컬럼이라 읽는다 or연산자
+		else if ((x==1) || (y==2) ) { // || 이거는 컬럼이라 읽는다 or연산자
 			System.out.println("x는 1이거나 y는 2이다");
+		}
+		else{
+			System.out.println("해당 사항 없음");
 		}
 
 	}
